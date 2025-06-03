fcb0869d4e25064a7d2c852331adae4d97379ef9
edeldev <edeldev25@gmail.com>
2025-06-02 18:15:34 -0600

    chore: resolve style moda image

--- components/ProductDetails/components/ModalImage.tsx
+++ components/ProductDetails/components/ModalImage.tsx
@@ -18,19 +18,13 @@ export const ModalImage = ({
           className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 cursor-zoom-out"
           onClick={() => setIsZoomOpen(false)}
         >
-          <motion.div
-            initial={{ scale: 0.7 }}
-            animate={{ scale: 1 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Image
-              src={imagePreview}
-              alt="Zoomed"
-              width={800}
-              height={800}
-              className="object-contain max-h-[95%] max-w-[95%]"
-            />
-          </motion.div>
+          <Image
+            src={imagePreview}
+            alt="Zoomed"
+            width={800}
+            height={800}
+            className="object-contain max-h-[95%] max-w-[95%]"
+          />
         </motion.div>
       )}
     </AnimatePresence>
